81f9b3b1a0c9818b05b7e33dc113747e41f88f82
max-student <max-student@maxtrain.com>
2020-02-10 09:10:48 -0500

    Assignment

---
+++ AssignmentProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AssignmentProject {
+    class Program {
+        static void Main(string[] args) {
+            var sum = 1;
+            for (var index = 1; index <= 10; index++) {
+                sum *= index;
+                }
+            Console.WriteLine($"Total is {sum}");
+
+            //alternative solution
+            sum = 1;
+            var nbr = 1;
+            while (nbr <= 10) {
+                sum *= nbr;
+                nbr++;
+                }
+            Console.WriteLine($"Total is {sum}");
+        }
+    }
+}
